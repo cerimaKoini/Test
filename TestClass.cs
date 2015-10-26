245978bf3622711b8a835f241f34a1139320b1cd
cerimaKoini <cerima.koini@gmx.at>
2015-10-26 16:18:26 +0100

    Create TestClass.cs
    This is a test class.

---
+++ TestClass.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace TestFile
+{
+  public class Person
+  {
+    private string name;
+    private int age;
+    
+    public string name
+    {
+      get;
+      set;
+    }
+    
+    public int Age
+    {
+      get;
+      set;
+    }
+  }
+}
